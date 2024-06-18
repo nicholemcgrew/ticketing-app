4919ee9b0e2da2e5dc35675c63c895b2cb9dca46
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-18 08:28:34 -0500

    added priority display

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,11 +3,11 @@ import PriorityDisplay from "./PriorityDisplay"
 import ProgressDisplay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
 
-const TicketCard = () => {
+const TicketCard = ({ ticket }) => {
 	return (
 		<div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
 			<div className="flex mb-3">
-				<PriorityDisplay />
+				<PriorityDisplay priority={ticket.priority} />
 				<div className="ml-auto">
 					<DeleteBlock />
 				</div>
@@ -19,15 +19,15 @@ const TicketCard = () => {
 			</p>
 			<div className="flex-grow"></div>
 			<div className="flex mt-2">
-					<div className="flex flex-col">
-						<p className="text-xs my-1">08/31/23 10:43pm</p>
-						<ProgressDisplay />
-					</div>
-                    <div className="ml-auto  flex items-end">
-				<StatusDisplay />
-                </div>
+				<div className="flex flex-col">
+					<p className="text-xs my-1">08/31/23 10:43pm</p>
+					<ProgressDisplay />
+				</div>
+				<div className="ml-auto  flex items-end">
+					<StatusDisplay />
 				</div>
 			</div>
+		</div>
 	)
 }
 
