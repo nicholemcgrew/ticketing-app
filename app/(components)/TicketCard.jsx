e49b86611fe66e3f8b0afedfb3b79a428611f9d2
Nichole McGrew <nicholemcgrew@gmail.com>
2024-06-18 08:45:41 -0500

    made everything dynamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,6 +4,22 @@ import ProgressDisplay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
 
 const TicketCard = ({ ticket }) => {
+
+	const formatTimeStamp = (timestamp) => {
+		const options = {
+			year: "numeric",
+			month: "2-digit",
+			day: "2-digit",
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: true,
+		}
+
+		const date = new Date(timestamp)
+		const formattedDate = date.toLocaleString("en-US", options)
+
+		return formattedDate
+	}
 	return (
 		<div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
 			<div className="flex mb-3">
@@ -12,19 +28,19 @@ const TicketCard = ({ ticket }) => {
 					<DeleteBlock />
 				</div>
 			</div>
-			<h4>Ticket Title</h4>
+			<h4>{ticket.title}</h4>
 			<hr className="h-px border-0 bg-page mb-2" />
 			<p className="whitespace-pre-wrap">
-				this is the ticket description! Please do this ticket.
+				{ticket.description}
 			</p>
 			<div className="flex-grow"></div>
 			<div className="flex mt-2">
 				<div className="flex flex-col">
-					<p className="text-xs my-1">08/31/23 10:43pm</p>
-					<ProgressDisplay />
+					<p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
+					<ProgressDisplay progress={ticket.progress}/>
 				</div>
 				<div className="ml-auto  flex items-end">
-					<StatusDisplay />
+					<StatusDisplay status={ticket.status} />
 				</div>
 			</div>
 		</div>
